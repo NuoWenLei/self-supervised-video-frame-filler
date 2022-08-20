a64a7fcde6aedc1a5f3ab0a17a96a096dcdbd56c
NuoWenLei <nuowen0612@gmail.com>
2022-08-20 07:51:09 -0400

    create tested data loader

--- bidir_model.py
+++ bidir_model.py
@@ -140,7 +140,8 @@ def bidirectional_conv_lstm_attention_bottleneck_model(
 	return tf.keras.models.Model(inputs = [prev_input, after_input], outputs = final_conv_prediction) 
 
 def frame_masking(frames, num_elements_front_and_back = 3):
-	X = []
+	X_prev = []
+	X_after = []
 	y = []
 	for i in range(num_elements_front_and_back, len(frames) - num_elements_front_and_back):
 
@@ -148,16 +149,19 @@ def frame_masking(frames, num_elements_front_and_back = 3):
 
 		# after frames specifically ordered to go from future to present (in that direction)
 		after_frames = frames[i+num_elements_front_and_back:i:-1]
-		X.append((previous_frames, after_frames))
+		X_prev.append(previous_frames)
+		X_after.append(after_frames)
+
 		y.append(frames[i])
 
-	return np.array(X), np.array(y)
+	return np.array(X_prev), np.array(X_after), np.array(y)
 
-def data_loader(video_path, num_elements_front_and_back = 3):
+def data_loader(video_path, image_height = 128, image_width = 128, num_elements_front_and_back = 3):
 	video_data = skvid_io.vread(video_path)
 	video_data_numpy = np.array(video_data)
-	print(video_data_numpy.shape)
-	X, y = frame_masking(video_data_numpy, num_elements_front_and_back=num_elements_front_and_back)
-	return X, y
+	video_data_resized = tf.image.resize_with_pad(video_data_numpy, image_height, image_width).numpy()
+	print(video_data_resized.shape)
+	X_prev, X_after, y = frame_masking(video_data_resized, num_elements_front_and_back=num_elements_front_and_back)
+	return X_prev, X_after, y
 
 
